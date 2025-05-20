33685c5943e9144543e0b271ee310334b180eaae
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-05-19 20:58:02 -0400

    details

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 const btnSearch = document.getElementById("btn");
 const card = document.querySelector(".card-creature");
+const footer = document.querySelector("footer");
 btnSearch.addEventListener("click", () => {
   card.style.display = "flex";
+  footer.style.marginTop = "0";
 });
