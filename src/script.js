53a5bfe9e9cbd5a07da13119c7db12c080218d66
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-05-19 22:00:01 -0400

    add svg logo

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 const btnSearch = document.getElementById("btn");
 const card = document.querySelector(".card-creature");
 const footer = document.querySelector("footer");
+const svg = document.querySelector(".svg");
 btnSearch.addEventListener("click", () => {
   card.style.display = "flex";
   footer.style.marginTop = "0";
+  svg.style.display = "none";
 });
